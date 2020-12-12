99f07d53210164baf05a11a25cc4712f6ca2ff7d
amitkumaharana <ak657maharana@gmail.com>
2020-12-12 23:11:33 +0530

    Daily employee wage

--- employeewage.sh
+++ employeewage.sh
@@ -1,10 +1,14 @@
 #!/bin/bash -x
 echo "Welcome to Employee Wage Computation Program"
+wage_per_hour=20
+full_day_hour=8
 var=$((RANDOM%2))
 if [ $var -eq 0 ]
 then
 	echo Employee is Present
+	daily_employee_wage=$((wage_per_hour*full_day_hour))
 else
 	echo Employee is Absent
+	daily_employee_wage=0
 fi
-
+echo $daily_employee_wage
