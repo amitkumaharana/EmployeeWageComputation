8ea8b7c51e78e15e942af317915898634d95f309
amitkumaharana <ak657maharana@gmail.com>
2020-12-12 23:26:22 +0530

    Part time employee and wage calculation

--- employeewage.sh
+++ employeewage.sh
@@ -2,10 +2,22 @@
 echo "Welcome to Employee Wage Computation Program"
 wage_per_hour=20
 full_day_hour=8
+part_time_hour=8
 var=$((RANDOM%2))
+part_time(){
+	a=$1
+	if [ $a -eq 0 ]
+	then
+		full_day_hour=16
+	else
+		full_day_hour=8
+	fi
+}
 if [ $var -eq 0 ]
 then
 	echo Employee is Present
+	var=$((RANDOM%2))
+	part_time $var
 	daily_employee_wage=$((wage_per_hour*full_day_hour))
 else
 	echo Employee is Absent
