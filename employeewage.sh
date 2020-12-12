6e079edb445d76f74367cd970af0e4be92ffbd40
amitkumaharana <ak657maharana@gmail.com>
2020-12-12 23:45:36 +0530

    Switch case statement used

--- employeewage.sh
+++ employeewage.sh
@@ -6,21 +6,17 @@ part_time_hour=8
 var=$((RANDOM%2))
 part_time(){
 	a=$1
-	if [ $a -eq 0 ]
-	then
-		full_day_hour=16
-	else
-		full_day_hour=8
-	fi
+	case $a in
+		0)full_day_hour=16;;
+		1)full_day_hour=8;;
+	esac
 }
-if [ $var -eq 0 ]
-then
-	echo Employee is Present
+case $var in
+	0)echo Employee is Present
 	var=$((RANDOM%2))
 	part_time $var
-	daily_employee_wage=$((wage_per_hour*full_day_hour))
-else
-	echo Employee is Absent
-	daily_employee_wage=0
-fi
+	daily_employee_wage=$((wage_per_hour*full_day_hour));;
+	1)echo Employee is Absent
+	daily_employee_wage=0;;
+esac
 echo $daily_employee_wage
