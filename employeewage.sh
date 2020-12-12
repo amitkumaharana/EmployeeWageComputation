c840287dca1d9ce01f6f5daaecbe0a17a1ff5568
amitkumaharana <ak657maharana@gmail.com>
2020-12-13 00:45:24 +0530

    Stored Day ,DAily wage and Total Wage

--- employeewage.sh
+++ employeewage.sh
@@ -1,11 +1,11 @@
-#!/bin/bash -x
+#!/bin/bash
 echo "Welcome to Employee Wage Computation Program"
 wage_per_hour=20
 full_day_hour=8
 part_time_hour=8
 monthly_wages=0
 total_hour=0
-i=0
+total_working_days=0
 work_hours(){
 	a=$1
 	case $a in
@@ -13,7 +13,7 @@ work_hours(){
 		1)full_day_hour=8;;
 	esac
 }
-while [ $i -le 20 -a $total_hour -le 100 ]
+while [ $total_working_days -le 20 -a $total_hour -le 100 ]
 do
 	var=$((RANDOM%2))
 	case $var in
@@ -21,13 +21,13 @@ do
 	var=$((RANDOM%2))
 	work_hours $var
 	daily_employee_wage=$((wage_per_hour*full_day_hour))
-	echo $daily_employee_wage
+	echo Todays working wages is $daily_employee_wage
 	total_hour=$((total_hour+full_day_hour));;
 	1)echo Employee is Absent
 	daily_employee_wage=0;;
 	esac
 	monthly_wages=$((monthly_wages+daily_employee_wage))
-	i=$((i+1))
+	total_working_days=$((total_working_days+1))
 done
-echo $monthly_wages
-echo $daily_employee_wage
+echo Monthly wages are $monthly_wages
+echo Total number of days worked are $total_working_days
